8d6d30b25adb2e52f8e50e904e3d5406b848a7ca
Nick Stewart <nick.stewart1993@gmail.com>
2020-11-10 21:44:26 +0000

    init project files

---
+++ loginapp/src/test/java/com/nickstewart/loginapp/LoginAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickstewart.loginapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
